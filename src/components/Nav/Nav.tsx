ffa5a72d1a40eda2239f14f496cc6e721237363b
dan1306 <81488150+dan1306@users.noreply.github.com>
2023-07-09 09:13:03 -0400

    nav css textbox animation

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import './Nav.css'
+import { BsWechat } from 'react-icons/bs';
 
 export const Nav = () => {
-  return (
-      <div className='NavContent'>
-          <div className='NavContent_logo'>
-              Nav
-          </div>
-          <div className='NavContent_status'>
-              <div>NavContent_status</div>
-              <div>NavContent_status</div>
-            
-          </div>
-      </div>
-  )
+    return (
+        <div className='NavContent'>
+            <div className='NavContent_logo'>
+                <div className='NavContent_logo--icon'>
+                    <BsWechat color='white' size={'3rem'} />
+                </div>
+                <h1 className='NavContent_logo--title'>
+                    ChatMeUP
+                </h1>
+            </div>
+            <div className='NavContent_status'>
+                <div>NavContent_status</div>
+                <div>NavContent_status</div>
+
+            </div>
+        </div>
+    )
 }
