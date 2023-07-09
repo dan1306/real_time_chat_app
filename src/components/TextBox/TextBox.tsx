afe1e801e79196ecc60b8dee42960a413f5b40e9
dan1306 <81488150+dan1306@users.noreply.github.com>
2023-07-09 08:11:32 -0400

    font change and send box hover animation

--- src/components/TextBox/TextBox.tsx
+++ src/components/TextBox/TextBox.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './TextBox.css'
+import {BsSendFill} from 'react-icons/bs';
 
 export const TextBox = () => {
     return (
         <div className='TextboxContent'>
             <input placeholder='Enter A Message' className='TextboxContent_input' />
             <div className='TextboxContent_send'>
-                send
+                <BsSendFill className='TextboxContent_send--BsSendFill' /> send
             </div>
         </div>
     )
