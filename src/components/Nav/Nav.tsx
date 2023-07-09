9f260adcfb8d06b1d7549ad1e3295c011db815fe
dan1306 <81488150+dan1306@users.noreply.github.com>
2023-07-09 10:28:37 -0400

    nav css

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,22 +1,31 @@
 import React from 'react'
 import './Nav.css'
 import { BsWechat } from 'react-icons/bs';
+import { BiSolidUser } from 'react-icons/bi'
+import { BsBellFill, BsPeopleFill } from 'react-icons/bs';
+// import {FaPeopleGroup} from 'react-icons/fa'
 
 export const Nav = () => {
     return (
         <div className='NavContent'>
             <div className='NavContent_logo'>
                 <div className='NavContent_logo--icon'>
-                    <BsWechat color='white' size={'3rem'} />
+                    <BsPeopleFill color='white' size={'3rem'} />
                 </div>
                 <h1 className='NavContent_logo--title'>
                     ChatMeUP
                 </h1>
             </div>
             <div className='NavContent_status'>
-                <div>NavContent_status</div>
-                <div>NavContent_status</div>
-
+                <div className='NavContent_status--BsWechat'>
+                    <BsWechat size={'1.5rem'} color={'#A2FF86'} />
+                </div>
+                <div className='NavContent_status--BsBellFill'>
+                    <BsBellFill size={'1.3rem'} color={'#B6EAFA'} />
+                </div>
+                <div className='NavContent_status--BiSolidUser'>
+                    <BiSolidUser size={'1.5rem'} color={'#C4DFDF'} />
+                </div>
             </div>
         </div>
     )
